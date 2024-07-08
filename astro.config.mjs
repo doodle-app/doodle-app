2d37e6fd0e98399672b86438a7378539c7336ad8
Joe Blau <im@joeblau.com>
2024-07-08 10:03:05 -0700

    feat: base to nothing

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://doodle-app.github.io",
+  base: "/",
   integrations: [
     react(),
     tailwind({
